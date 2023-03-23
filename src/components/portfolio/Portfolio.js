d1e22008f9774fa70a824461e7794ceae7611a8d
vasanthmn1 <mnvasanth4@gmail.com>
2023-03-23 11:39:26 +0530

    ok

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,34 +1,38 @@
 import React from 'react'
 import img from '../../assate/bg.jpg'
+import projet1 from '../../assate/money.png'
+
 
 import './portfolio.css'
 export const Portfolio = () => {
     const data = [
         {
             id: "1",
-            gitupUrl: "https://github.com/vasanthmn1",
-            websiteUrl: ""
+            title: "Money mangement app",
+            gitupUrl: "https://github.com/vasanthmn1/capstone-project-client",
+            websiteUrl: "https://admirable-pudding-b31006.netlify.app/",
+            img: projet1,
         },
-        {
-            id: "2",
-            gitupUrl: "https://github.com/vasanthmn1",
-            websiteUrl: ""
-        },
-        {
-            id: "3",
-            gitupUrl: "https://github.com/vasanthmn1",
-            websiteUrl: ""
-        },
-        {
-            id: "4",
-            gitupUrl: "https://github.com/vasanthmn1",
-            websiteUrl: ""
-        },
-        {
-            id: "5",
-            gitupUrl: "https://github.com/vasanthmn1",
-            websiteUrl: ""
-        },
+        // {
+        //     id: "2",
+        //     gitupUrl: "https://github.com/vasanthmn1",
+        //     websiteUrl: ""
+        // },
+        // {
+        //     id: "3",
+        //     gitupUrl: "https://github.com/vasanthmn1",
+        //     websiteUrl: ""
+        // },
+        // {
+        //     id: "4",
+        //     gitupUrl: "https://github.com/vasanthmn1",
+        //     websiteUrl: ""
+        // },
+        // {
+        //     id: "5",
+        //     gitupUrl: "https://github.com/vasanthmn1",
+        //     websiteUrl: ""
+        // },
     ]
 
     return (
@@ -41,12 +45,12 @@ export const Portfolio = () => {
                         return (
                             <div className='col-lg-4 col-md-6 mb-5' key={idx}>
                                 <div className="card portfolio__card ">
-                                    <img src={img} className="card-img-top portfolio__img" alt="img" />
+                                    <img src={val.img} className="card-img-top portfolio__img" alt="img" />
                                     <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
+                                        <h5 className="card-title">{val.title}</h5>
                                         <div className='portfolio__btns'>
                                             <a href={val.gitupUrl} target='_blank' className='githup__btn'>Githup</a>
-                                            <a href='#' target='_blank' className='website__btn'>Website</a>
+                                            <a href={val.websiteUrl} target='_blank' className='website__btn'>Website</a>
                                         </div>
                                     </div>
                                 </div>
