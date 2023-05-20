e761969acb4d13ea40ac397f94296628db61db62
vasanthmn1 <mnvasanth4@gmail.com>
2023-05-20 18:05:00 +0530

    chnage

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { TopContainer } from './components/topcontainer/TopContainer';
 function App() {
   return (
     <div className="App">
+
       <TopContainer />
 
       <Nav />
@@ -17,6 +18,7 @@ function App() {
       <Portfolio />
       <Contact />
       <Footer />
+
     </div>
   );
 }
