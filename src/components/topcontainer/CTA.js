21c3d1aba3c7b7bd4362c3525cb48d86abd424f2
vasanthmn1 <mnvasanth4@gmail.com>
2023-07-22 14:06:13 +0530

    resume link

--- src/components/topcontainer/CTA.js
+++ src/components/topcontainer/CTA.js
@@ -4,7 +4,7 @@ import CV from '../../assate/cv.pdf'
 export const CTA = () => {
     return (
         <div className='cta'>
-            <a className='button' target='_blank' href={'https://drive.google.com/file/d/1RkyC72fG1u9heaJDCs5nNXJrqbdvk-LX/view?usp=sharing'} download>download CV</a>
+            <a className='button' target='_blank' href={'https://drive.google.com/file/d/1CcDl6NtAEZjfteBiB93lLH-pIediRKzp/view'} download>download CV</a>
             <a className=' cv__btn' href='#contact'>Contact Me</a>
         </div>
     )
