711eafbd11bfcfdca89eb7464bc31b39ebb3b66f
vasanth <mnvasanth4@gmail.com>
2024-03-07 21:18:15 +0530

    portfolio data change separate file

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,130 +1,15 @@
 import React, { useState } from 'react';
-import projet1 from '../../assate/money.png';
-import projet2 from '../../assate/interior.jpg';
-import projet3 from '../../assate/blog.jpg';
-import projet4 from '../../assate/astrology.jpg';
-import projet5 from '../../assate/chat.jpg';
-import projet6 from '../../assate/gunshop.jpg';
+
 import './portfolio.css';
 import { useCopyToClipboard } from 'react-use';
 import { FaCopy } from 'react-icons/fa'
-import { Toaster, toast } from 'react-hot-toast';
+import { toast } from 'react-hot-toast';
+import { portfolio_data } from '../../data/portfolio_data';
 export const Portfolio = () => {
     const [open, setOpen] = useState('');
     const [state, copyToClipboard] = useCopyToClipboard();
 
-    const data = [
-        {
-            id: '1',
-            title: 'Money management app',
-            gitupUrl: [
-                {
-                    frontend: 'https://github.com/vasanthmn1/capstone-project-client',
-                },
-                {
-                    backend: 'https://github.com/vasanthmn1/capstone-project-backend',
-                },
-            ],
-            websiteUrl: 'https://admirable-pudding-b31006.netlify.app/',
-            img: projet1,
-            optiontitle: "User",
-            admilEmail: " 1@gmail.com",
-            admilpassword: " 1@gmail.com"
-        },
-        {
-            id: '2',
-            title: 'Interior',
-            gitupUrl: [
-                {
-                    frontend: 'https://github.com/vasanthmn1/interior-frontendapp',
-                },
-                {
-                    backend: 'https://github.com/vasanthmn1/interior-bakend',
-                },
-            ],
-            websiteUrl: 'https://master--chic-pithivier-376c30.netlify.app/',
-            img: projet2,
-            optiontitle: "Admin",
-            admilEmail: "admin@gmail.com",
-            admilpassword: " admin1234",
-            optionpath: "...../admin",
-            optioncode: "1122",
 
-        },
-        {
-            id: '3',
-            title: 'Blog',
-            gitupUrl: [
-                {
-                    frontend: 'https://github.com/vasanthmn1/blog-frontend',
-                },
-                {
-                    backend: 'https://github.com/vasanthmn1/backend-blog',
-                },
-            ],
-            websiteUrl: 'https://spontaneous-mooncake-060ef3.netlify.app/',
-            img: projet3,
-            optiontitle: "User",
-            admilEmail: " 1@gmail.com",
-            admilpassword: " 1@gmail.com"
-        },
-        {
-            id: '4',
-            title: 'Astrology',
-            gitupUrl: [
-                {
-                    frontend: 'https://github.com/vasanthmn1/frontend-astrology',
-                },
-                {
-                    backend: 'https://github.com/vasanthmn1/backend-astrology',
-                },
-            ],
-            websiteUrl: 'https://celebrated-moonbeam-21c681.netlify.app/',
-            img: projet4,
-            optiontitle: "Admin",
-            admilEmail: "admin@gmail.com",
-            admilpassword: " admin1234",
-            optiontitle2: "User",
-            userEmail: "1@gmail.com",
-            userpassword: "1@gmail.com"
-        },
-        {
-            id: '5',
-            title: 'Mini Chat',
-            gitupUrl: [
-                {
-                    frontend: 'https://github.com/vasanthmn1/minichat-frontend',
-                },
-                {
-                    backend: 'https://github.com/vasanthmn1/minichat-backend',
-                },
-            ],
-            websiteUrl: 'https://clever-chaja-38768f.netlify.app/',
-            img: projet5,
-            // optiontitle: "Admin",
-
-
-            // admilEmail: "Eamil: admin@gmail.com",
-            // admilpassword: "Password:  admin1234"
-        },
-        {
-            id: '6',
-            title: 'OnGuns',
-            gitupUrl: [
-                {
-                    frontend: 'https://github.com/vasanthmn1/frontend-gunshop',
-                },
-                {
-                    backend: 'https://github.com/vasanthmn1/backend-gunshop-stripe',
-                },
-            ],
-            websiteUrl: 'https://magical-melba-799351.netlify.app/',
-            img: projet6,
-            admilEmail: "admin@gmail.com",
-            admilpassword: " admin1234"
-
-        },
-    ];
     // let texts = "Copy this";
     const handleClick = (idx) => {
         setOpen(open === idx ? '' : idx);
@@ -142,7 +27,7 @@ export const Portfolio = () => {
                     <span>P</span>rojects
                 </h1>
 
-                {data.map((val, idx) => (
+                {portfolio_data.map((val, idx) => (
                     <div
                         className="col-lg-4 col-md-6 mb-5 d-flex justify-content-center align-items-center"
                         key={idx}
@@ -150,7 +35,7 @@ export const Portfolio = () => {
                         <div className=" portfolio__card">
                             <div className='img__box'>
                                 <img
-                 loading="lazy"
+                                    loading="lazy"
                                     src={val.img}
                                     className="card-img-top portfolio__img"
                                     alt="img"
@@ -310,26 +195,4 @@ export const Portfolio = () => {
     );
 };
 
-// export default Portfolio;
 
-{/* {
-                                        val.optiontitle &&
-                                        <select className='select'  >
-                                            <option >{val.optiontitle}</option>
-                                            {val.optionpath && <option >{val.optionpath}</option>}
-                                            {/* && <button> <FaCopyright /></button> */}
-{/* <span className='copybtn'>
-                                        <FaRegCopyright />
-                                    </span>
-                                    <option > <FaRegCopyright className='copybtn' />  {val.admilEmail}   <span className='copybtn'>
-                                        <FaRegCopyright />
-                                    </span> </option>
-                                    <option>  {val.admilpassword}</option>
-                                    {val.admilpassword && <option >    <span className='copybtn'>
-                                        <FaRegCopyright />
-                                    </span></option>}
-                                    {val.optioncode && <option >{val.optioncode}</option>} */}
-
-
-{/* </select>
-                                    } */}
---
+++ src/data/portfolio_data.js
@@ -0,0 +1,119 @@
+import project1 from '../assate/money.png';
+import project2 from '../assate/interior.jpg';
+import project3 from '../assate/blog.jpg';
+import project4 from '../assate/astrology.jpg';
+import project5 from '../assate/chat.jpg';
+import project6 from '../assate/gunshop.jpg';
+
+export const portfolio_data = [
+    {
+        id: '1',
+        title: 'Money management app',
+        gitupUrl: [
+            {
+                frontend: 'https://github.com/vasanthmn1/capstone-project-client',
+            },
+            {
+                backend: 'https://github.com/vasanthmn1/capstone-project-backend',
+            },
+        ],
+        websiteUrl: 'https://admirable-pudding-b31006.netlify.app/',
+        img: project1,
+        optiontitle: "User",
+        admilEmail: " 1@gmail.com",
+        admilpassword: " 1@gmail.com"
+    },
+    {
+        id: '2',
+        title: 'Interior',
+        gitupUrl: [
+            {
+                frontend: 'https://github.com/vasanthmn1/interior-frontendapp',
+            },
+            {
+                backend: 'https://github.com/vasanthmn1/interior-bakend',
+            },
+        ],
+        websiteUrl: 'https://master--chic-pithivier-376c30.netlify.app/',
+        img: project2,
+        optiontitle: "Admin",
+        admilEmail: "admin@gmail.com",
+        admilpassword: " admin1234",
+        optionpath: "...../admin",
+        optioncode: "1122",
+
+    },
+    {
+        id: '3',
+        title: 'Blog',
+        gitupUrl: [
+            {
+                frontend: 'https://github.com/vasanthmn1/blog-frontend',
+            },
+            {
+                backend: 'https://github.com/vasanthmn1/backend-blog',
+            },
+        ],
+        websiteUrl: 'https://spontaneous-mooncake-060ef3.netlify.app/',
+        img: project3,
+        optiontitle: "User",
+        admilEmail: " 1@gmail.com",
+        admilpassword: " 1@gmail.com"
+    },
+    {
+        id: '4',
+        title: 'Astrology',
+        gitupUrl: [
+            {
+                frontend: 'https://github.com/vasanthmn1/frontend-astrology',
+            },
+            {
+                backend: 'https://github.com/vasanthmn1/backend-astrology',
+            },
+        ],
+        websiteUrl: 'https://celebrated-moonbeam-21c681.netlify.app/',
+        img: project4,
+        optiontitle: "Admin",
+        admilEmail: "admin@gmail.com",
+        admilpassword: " admin1234",
+        optiontitle2: "User",
+        userEmail: "1@gmail.com",
+        userpassword: "1@gmail.com"
+    },
+    {
+        id: '5',
+        title: 'Mini Chat',
+        gitupUrl: [
+            {
+                frontend: 'https://github.com/vasanthmn1/minichat-frontend',
+            },
+            {
+                backend: 'https://github.com/vasanthmn1/minichat-backend',
+            },
+        ],
+        websiteUrl: 'https://clever-chaja-38768f.netlify.app/',
+        img: project5,
+        // optiontitle: "Admin",
+
+
+        // admilEmail: "Eamil: admin@gmail.com",
+        // admilpassword: "Password:  admin1234"
+    },
+    {
+        id: '6',
+        title: 'OnGuns',
+        gitupUrl: [
+            {
+                frontend: 'https://github.com/vasanthmn1/frontend-gunshop',
+            },
+            {
+                backend: 'https://github.com/vasanthmn1/backend-gunshop-stripe',
+            },
+        ],
+        websiteUrl: 'https://magical-melba-799351.netlify.app/',
+        img: project6,
+        admilEmail: "admin@gmail.com",
+        admilpassword: " admin1234"
+
+    },
+];
